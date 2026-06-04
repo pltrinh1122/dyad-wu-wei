822b0df80901b9c5790c4ff79ae5428fc889877e
pltrinh1122 <pltrinh1122@gmail.com>
2026-06-04 07:19:44 -0700

    fix: revert synchronous github API call in NBA sorting

--- kernel/daemon_nba.py
+++ kernel/daemon_nba.py
@@ -165,23 +165,19 @@ class NBADaemon:
                 except Exception:
                     pass  # If API fails, proceed with unfiltered list
                 
-                from kernel.daemon_strategic import find_parent_path_id
                 prioritized_set = set(prioritized_ids)
                 matched_items = []
                 unmatched_items = []
                 for item in backlog_items:
                     num_str = str(item.get("number", ""))
-                    parent_id = str(find_parent_path_id(num_str) or "")
-                    if parent_id in prioritized_set:
-                        matched_items.append((prioritized_ids.index(parent_id), item))
+                    if num_str in prioritized_set:
+                        matched_items.append(item)
                     else:
                         unmatched_items.append(item)
                 
-                matched_items.sort(key=lambda x: (x[0], x[1].get("number", 0)))
-                matched_items = [x[1] for x in matched_items]
-                unmatched_items.sort(key=lambda x: x.get("number", 0))
-                
+                matched_items.sort(key=lambda x: prioritized_ids.index(str(x.get("number", ""))))
                 backlog_items = matched_items + unmatched_items
+
 
             return {
                 "type": "path_switching",
